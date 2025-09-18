9a4353a60a6d29174e2fbce7ba50ccf876c1c343
sjb-iv <samuelburnsiv@gmail.com>
2025-09-18 14:12:21 -0400

    updates

--- index.js
+++ index.js
@@ -6,16 +6,26 @@ const tabButtons = document.querySelectorAll(".tab-btn")
 
 
 function openTab(evt, tabId){
-    const tabs = document.querySelectorAll(".tab-content");
-    const tabButtons = document.querySelectorAll(".tab-btn");
+    const tabContents = document.querySelectorAll('.tab-content');
+    tabContents.forEach(tc => tc.classList.remove('active'));
 
-    tabs.forEach(tab => tab.classList.remove("active"));
-    tabButtons.forEach(btn => btn.classList.remove("tab-active"));
+    const tabBtns = document.querySelectorAll('.tab-btn');
+    tabBtns.forEach(btn => btn.classList.remove('tab-active'));
 
-    document.getElementById(tabId).classList.add("active");
-    evt.target.classList.add("tab-active");
+    document.getElementById(tabId).classList.add('active');
+    evt.currentTarget.classList.add('tab-active');
+
+    // Store the active tab in sessionStorage
+    sessionStorage.setItem('activeTab', tabId);
     
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+    // Default to About tab on first load
+    const lastTab = sessionStorage.getItem('activeTab') || 'about';
+    const tabBtn = document.querySelector(`.tab-btn[onclick*="${lastTab}"]`);
+    if (tabBtn) tabBtn.click();
+});
 
 // window.addEventListener("DOMContentLoaded", () => {
 //   const hash = window.location.hash.substring(1); // remove the #
